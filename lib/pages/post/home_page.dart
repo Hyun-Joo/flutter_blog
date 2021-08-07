ccd212fc0f04745c36940716f72b36ad1ba1dcb1
Hyun-Joo <dlguswn714@gmail.com>
2021-08-07 19:49:05 +0900

    ~ 글쓰기, 회원정보보기, 글수정 화면 세팅

--- lib/pages/post/home_page.dart
+++ lib/pages/post/home_page.dart
@@ -2,6 +2,9 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_blog/constants/size.dart';
 import 'package:flutter_blog/pages/post/detail_page.dart';
+import 'package:flutter_blog/pages/post/write_page.dart';
+import 'package:flutter_blog/pages/user/login_page.dart';
+import 'package:flutter_blog/pages/user/user_info.dart';
 import 'package:get/get.dart';
 
 class HomePage extends StatelessWidget {
@@ -40,7 +43,23 @@ class HomePage extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               TextButton(
-                onPressed: (){},
+                onPressed: () {
+                  Get.to(WritePage());
+                },
+                child: Text(
+                  '글쓰기',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black54
+                  )
+                )
+              ),
+              Divider(),
+              TextButton(
+                onPressed: () {
+                  Get.to(UserInfo());
+                },
                 child: Text(
                   '회원정보보기',
                   style: TextStyle(
@@ -52,7 +71,9 @@ class HomePage extends StatelessWidget {
               ),
               Divider(),
               TextButton(
-                onPressed: (){},
+                onPressed: () {
+                  Get.to(LoginPage());
+                },
                 child: Text(
                   '로그아웃',
                   style: TextStyle(
